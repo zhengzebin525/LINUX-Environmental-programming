386270464f7ae548e089ee07e0f8efc2d9f3a3a1
郑泽彬 <44545653+zhengzebin525@users.noreply.github.com>
2019-01-12 12:21:07 +0800

    基于UDP协议的客户机和服务器
    基于UDP协议，模拟客户机和服务器，将客户机发来的消息原封不动反弹回去

---
+++ 网络编程/服务器和客户机-UDP/client.c
@@ -0,0 +1,74 @@
+﻿#include <time.h>
+#include <errno.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <strings.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <linux/fb.h>
+#include <linux/un.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+//编写一个基于 UDP 的镜子服务器，将客户端发送过来的数据照原样反弹回去。
+
+int main(int argc,char *argv[])
+{
+	//创建UDP通信端点，SOCK_DGRAM表示采用UDP协议
+	/* UDP协议特征：
+	1、发送的数据，肯能会无序到达接收端
+	2、数据可能丢失，丢失了不会采取任何不久措施，而且接受端也不知道有数据丢失
+	3、数据有尺寸大小限制 */
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(fd == -1)
+	{
+		perror("socket()发生错误\n");
+		exit(0);
+	}
+	
+	struct sockaddr_in addr;
+	socklen_t len = sizeof(addr);
+	bzero(&addr,len);
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr(argv[1]);
+	addr.sin_port = htons(atoi(argv[2]));
+	
+	char buf[200];
+	while(1)
+	{
+		bzero(buf, 200);
+		fgets(buf,200, stdin);
+		
+		/* 将数据传给对方主机，UDP协议中socket可以不进行连接操作
+		参数4：是一个标志位，一般设为0
+		参数5：对方主机的套接字标准地址
+		参数6：对方主机地址的长度 */
+		sendto(fd, buf, strlen(buf), 0, (struct sockaddr*)&addr, len);
+		
+     	bzero(buf, 200);
+		
+		/* 接收来自cliaddr地址的数据，函数中有一个地址的参数，可以自己手动输入
+		参数1：套接字描述符
+		参数4：调用方式标志位，一般为0
+		参数5：目的套接字的标准地址
+		参数6：目的地址的长度 */
+		recvfrom(fd, buf,200, 0, (struct sockaddr*)&addr,&len);
+		
+		printf("接受到服务器信息：%s",buf);
+	}
+	
+	return 0;
+	
+}
---
+++ 网络编程/服务器和客户机-UDP/server.c
@@ -0,0 +1,77 @@
+﻿#include <time.h>
+#include <errno.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <strings.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <linux/fb.h>
+#include <linux/un.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
+
+int main(int argc,char *argv[])
+{
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(fd == -1)
+	{
+		perror("socket()发生错误\n");
+		exit(0);
+	}
+	
+	struct sockaddr_in addr;
+	socklen_t len = sizeof(addr);
+	bzero(&addr,len);
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(50001);
+	
+	int bind_ret = bind(fd, (struct sockaddr*)&addr, len);
+	if(bind_ret == -1)
+	{
+		perror("地址绑定失败！\n");
+		exit(0);
+	}
+	
+	struct sockaddr_in cliaddr;
+	socklen_t clilen = sizeof(cliaddr);
+	bzero(&cliaddr, clilen);
+	
+	char buf[200];
+	while(1)
+	{
+		bzero(buf, 200);
+		
+		recvfrom(fd, buf,200, 0, (struct sockaddr*)&cliaddr,&clilen);
+		
+		printf("接收到来自[%s:%hu]的数据：%s",
+				inet_ntoa(cliaddr.sin_addr),
+				ntohs(cliaddr.sin_port),
+				buf);
+				
+		sendto(fd, buf, strlen(buf), 0, (struct sockaddr*)&cliaddr, clilen);
+				
+	}
+	
+	return 0;
+	
+}
+
+
+
+
+
